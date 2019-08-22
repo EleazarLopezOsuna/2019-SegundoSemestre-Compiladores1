6d0745bf15685713f93e6f5647bc356f8ecc3f33
"Jared Osuna" <"jaredtl023@gmail.com">
2019-08-22 14:49:34 -0600

    Update de los generadores

--- src/Generadores/Lexico.bat
+++ src/Generadores/Lexico.bat
@@ -1,17 +1,17 @@
 SET JAVA_HOME = "C:\Program Files\Java\jdk-11.0.2\bin";
 SET PATH =% JAVA_HOME%;%PATH%;
 SET CLASSPATH = %JAVA_HOME%;
-cd C:\Users\USER\Documents\NetBeansProjects\Proyecto1_Compiladores_SegundoSemestre_2019\src\Analizadores\HTML
+cd C:\Users\USER\Desktop\USAC - CYS\2019 - Segundo Semestre\Compiladores 1\Proyecto 1\2019-SegundoSemestre-Compiladores1\src\Analizadores\HTML
 java -jar C:\Users\USER\Desktop\Analizadores\jflex-1.7.0\lib\jflex-full-1.7.0.jar Lexico.jflex
 
 SET JAVA_HOME = "C:\Program Files\Java\jdk-11.0.2\bin";
 SET PATH =% JAVA_HOME%;%PATH%;
 SET CLASSPATH = %JAVA_HOME%;
-cd C:\Users\USER\Documents\NetBeansProjects\Proyecto1_Compiladores_SegundoSemestre_2019\src\Analizadores\CSS
+cd C:\Users\USER\Desktop\USAC - CYS\2019 - Segundo Semestre\Compiladores 1\Proyecto 1\2019-SegundoSemestre-Compiladores1\src\Analizadores\CSS
 java -jar C:\Users\USER\Desktop\Analizadores\jflex-1.7.0\lib\jflex-full-1.7.0.jar Lexico.jflex
 
 SET JAVA_HOME = "C:\Program Files\Java\jdk-11.0.2\bin";
 SET PATH =% JAVA_HOME%;%PATH%;
 SET CLASSPATH = %JAVA_HOME%;
-cd C:\Users\USER\Documents\NetBeansProjects\Proyecto1_Compiladores_SegundoSemestre_2019\src\Analizadores\UFE
+cd C:\Users\USER\Desktop\USAC - CYS\2019 - Segundo Semestre\Compiladores 1\Proyecto 1\2019-SegundoSemestre-Compiladores1\src\Analizadores\UFE
 java -jar C:\Users\USER\Desktop\Analizadores\jflex-1.7.0\lib\jflex-full-1.7.0.jar Lexico.jflex
--- src/Generadores/Sintactico.bat
+++ src/Generadores/Sintactico.bat
@@ -1,5 +1,5 @@
 SET JAVA_HOME = "C:\Program Files\Java\jdk-11.0.2\bin";
 SET PATH =% JAVA_HOME%;%PATH%;
 SET CLASSPATH = %JAVA_HOME%;
-cd C:\Users\USER\Documents\NetBeansProjects\Proyecto1_Compiladores_SegundoSemestre_2019\src\Analizadores\HTML
+cd C:\Users\USER\Desktop\USAC - CYS\2019 - Segundo Semestre\Compiladores 1\Proyecto 1\2019-SegundoSemestre-Compiladores1\src\Analizadores\HTML
 java -jar C:\Users\USER\Desktop\Analizadores\java-cup-11b.jar -parser Analisis_Sintactico -symbols Simbolos Sintactico.cup
